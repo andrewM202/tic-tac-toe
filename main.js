5797de5ae54049a63951e1f92c046ac0f5acb5be
Iraidalis Lugo Cortes <lugoiraidalis@gmail.com>
2020-11-09 15:36:07 -0500

    Link Javascript file to HTML-Added event listeners to each tiles

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+function change(element) {
+   if (element.textContent === ""){
+      element.textContent = "X";
+    }else if (element.textContent === "X") {
+      element.textContent = "O";
+    }else {
+      element.textContent = "";
+    }
+}
 
+document.querySelectorAll('.block').forEach(function (element) {
+   element.addEventListener('click', change(element));
+});
