0b46170bb4f0624691113a510b45747d3ca622d3
Iraidalis Lugo Cortes <lugoiraidalis@gmail.com>
2020-11-10 16:22:53 -0500

    Fix style of the code

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
 /* Create a random generator for X or O */
 let randomXorO = Math.random();
-if(randomXorO < 0.5) {
+if (randomXorO < 0.5) {
   randomXorO = "X";
 } else {
   randomXorO = "O";
@@ -11,7 +10,8 @@ if(randomXorO < 0.5) {
 let firstTurn = true;
 /* Create a variable to switch between X and O. This variable is opposite of randomXorO */
 let OsTurn;
-if(randomXorO === "X") { /* If randomXorO is X, then O is up for second turn, hence OsTurn is true */
+if (randomXorO === "X") {
+  /* If randomXorO is X, then O is up for second turn, hence OsTurn is true */
   OsTurn = true;
 } else {
   OsTurn = false;
@@ -20,22 +20,25 @@ if(randomXorO === "X") { /* If randomXorO is X, then O is up for second turn, he
 function change(element) {
   let currentPlayer = document.querySelector('.current-player p');
 
-   if(firstTurn) { /* If its first turn, have it be random whether O or X goes first */
-     if (this.firstChild.textContent === "") { /* It checks this.firstChild since that is the <p> */
-        this.firstChild.textContent = randomXorO;
-        firstTurn = false; /* After first turn is over, switch firstTurn to false */
-      }
-    } else if (this.firstChild.textContent === "") { /* If its not first turn, alternate between X and O */
-      if(OsTurn === true) {
-        this.firstChild.textContent = "O";
-        OsTurn = false;
-        currentPlayer.textContent = "Current player: O";
-      } else {
-        this.firstChild.textContent = "X";
-        OsTurn =  true;
-        currentPlayer.textContent = "Current player: X";
-      }
+  if (firstTurn) {
+    /* If its first turn, have it be random whether O or X goes first */
+    if (this.firstChild.textContent === "") {
+      /* It checks this.firstChild since that is the <p> */
+      this.firstChild.textContent = randomXorO;
+      firstTurn = false; /* After first turn is over, switch firstTurn to false */
     }
+  } else if (this.firstChild.textContent === "") {
+    /* If its not first turn, alternate between X and O */
+    if (OsTurn === true) {
+      this.firstChild.textContent = "O";
+      OsTurn = false;
+      currentPlayer.textContent = "Current player: O";
+    } else {
+      this.firstChild.textContent = "X";
+      OsTurn = true;
+      currentPlayer.textContent = "Current player: X";
+    }
+  }
 }
 
 /* Add event listeners and put in first player below */
@@ -45,7 +48,7 @@ window.onload = function() {
   currentPlayer.textContent = "Current player: " + randomXorO;
 
   /* Add event listener to each block in grid */
-  document.querySelectorAll('.block').forEach(function (element) {
-     element.addEventListener('click', change);
+  document.querySelectorAll('.block').forEach(function(element) {
+    element.addEventListener('click', change);
   });
 }
