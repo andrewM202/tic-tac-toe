c233d0ec8e2006606f6842b3f1022d29c828a1d5
Andrew Matt <andrewm1246@gmail.com>
2020-11-10 17:06:19 -0500

    Changed JS to match new HTML tabbing

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+
 /* Create a random generator for X or O */
 let randomXorO = Math.random();
-if (randomXorO < 0.5) {
+if(randomXorO < 0.5) {
   randomXorO = "X";
 } else {
   randomXorO = "O";
@@ -10,8 +11,7 @@ if (randomXorO < 0.5) {
 let firstTurn = true;
 /* Create a variable to switch between X and O. This variable is opposite of randomXorO */
 let OsTurn;
-if (randomXorO === "X") {
-  /* If randomXorO is X, then O is up for second turn, hence OsTurn is true */
+if(randomXorO === "X") { /* If randomXorO is X, then O is up for second turn, hence OsTurn is true */
   OsTurn = true;
 } else {
   OsTurn = false;
@@ -19,29 +19,29 @@ if (randomXorO === "X") {
 
 function change(element) {
   let currentPlayer = document.querySelector('.current-player p');
+  let pEl = this.querySelector('p');
 
    if(firstTurn) { /* If its first turn, have it be random whether O or X goes first */
-     if (this.firstChild.textContent === "") { /* It checks this.firstChild since that is the <p> */
-        this.firstChild.textContent = randomXorO;
+     if (pEl.textContent === "") { /* It checks this.firstChild since that is the <p> */
+        pEl.textContent = randomXorO;
         firstTurn = false; /* After first turn is over, switch firstTurn to false */
         if(randomXorO === "X") {
-          currentPlayer.textContent = "Current player: O"
+          currentPlayer.textContent = "Current player: O";
         } else {
           currentPlayer.textContent = "Current player: X";
         }
       }
-    } else if (this.firstChild.textContent === "") { /* If its not first turn, alternate between X and O */
+    } else if (pEl.textContent === "") { /* If its not first turn, alternate between X and O */
       if(OsTurn === true) {
-        this.firstChild.textContent = "O";
+        pEl.textContent = "O";
         OsTurn = false;
         currentPlayer.textContent = "Current player: X";
       } else {
-        this.firstChild.textContent = "X";
+        pEl.textContent = "X";
         OsTurn =  true;
         currentPlayer.textContent = "Current player: O";
       }
     }
-  }
 }
 
 /* Add event listeners and put in first player below */
@@ -51,7 +51,7 @@ window.onload = function() {
   currentPlayer.textContent = "Current player: " + randomXorO;
 
   /* Add event listener to each block in grid */
-  document.querySelectorAll('.block').forEach(function(element) {
-    element.addEventListener('click', change);
+  document.querySelectorAll('.block').forEach(function (element) {
+     element.addEventListener('click', change);
   });
 }
